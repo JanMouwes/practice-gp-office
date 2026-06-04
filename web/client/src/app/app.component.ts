192d424fba8245f0796ea265517c7bdb1f5e4b90
JanMouwes <jan.mouwes@gmail.com>
2026-06-04 16:02:45 +0200

    update angular 20 -> 21

--- web/client/src/app/app.component.ts
+++ web/client/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
+
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./common/navbar/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
