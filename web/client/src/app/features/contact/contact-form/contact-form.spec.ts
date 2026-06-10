62097694272a9c9e30a7cedf6e620cb4fd2e9942
JanMouwes <jan.mouwes@gmail.com>
2026-06-10 15:50:09 +0200

    experiment with signal forms: implement contact form binding & submission, add some tests

--- web/client/src/app/features/contact/contact-form/contact-form.spec.ts
+++ web/client/src/app/features/contact/contact-form/contact-form.spec.ts
@@ -2,20 +2,27 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ContactForm} from './contact-form';
 import {ContactService, DIToken} from '../contact-service/contact-service';
-import {beforeEach, describe, expect, it, Mocked, vi} from "vitest";
+import {beforeEach, describe, expect, it, vi} from "vitest";
+import {By} from "@angular/platform-browser";
 import {ValidEmail} from "../contact-service/valid-email";
 
 describe('ContactForm', () => {
+  const ContactServiceSpy = vi.fn(class implements ContactService {
+    sendContactMessage = vi.fn();
+  });
+
   let component: ContactForm;
   let fixture: ComponentFixture<ContactForm>;
-  let contactServiceSpy: Mocked<ContactService> = createContactServiceSpy();
+  let serviceSpy: ContactService;
 
   beforeEach(async () => {
-    contactServiceSpy = createContactServiceSpy();
+    serviceSpy = new ContactServiceSpy();
 
     await TestBed.configureTestingModule({
       imports: [ContactForm],
-      providers: [{provide: DIToken, useValue: contactServiceSpy}]
+      providers: [
+        {provide: DIToken, useValue: serviceSpy},
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ContactForm);
@@ -28,40 +35,52 @@ describe('ContactForm', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('sendContactMessage', () => {
+  describe('send button', () => {
+    function getSendButton() {
+      return fixture.debugElement.query(By.css('[type=submit]'));
+    }
+
+    it('should call handler when clicked', async () => {
+      const handler = vi.spyOn(component, 'trySendMessage');
+      const btn = getSendButton();
+
+      btn.nativeElement.click();
+      await fixture.whenStable();
+
+      expect(handler).toHaveBeenCalledOnce();
+    });
+  });
+
+  describe('handleSendClicked', () => {
     it('should call ContactService with valid data', async () => {
+      const notifySpy = vi.spyOn(component, 'notifySuccess')
       const testData = {
         name: 'test name',
         email: 'test@email.com',
         message: 'test message',
       };
 
-      const validatedEmail = ValidEmail.parseEmail(testData.email);
-      await component.sendContactMessage(testData.name, testData.email, testData.message);
+      const expectedEmail = ValidEmail.parseEmail(testData.email);
+      await component.trySendMessage(testData.name, testData.email, testData.message);
 
-      expect(contactServiceSpy.sendContactMessage).toHaveBeenCalledExactlyOnceWith(
+      expect(serviceSpy.sendContactMessage).toHaveBeenCalledExactlyOnceWith(
         testData.name,
-        validatedEmail,
+        expectedEmail,
         testData.message
       );
+      expect(notifySpy).toHaveBeenCalledOnce()
     });
     it.each([
         {name: '', email: 'test@email.com', message: 'test message'},
         {name: 'valid name', email: 'test@email.com', message: ''},
         {name: 'valid name', email: '', message: 'valid test message'},
       ]
-    )('should not call ContactService with invalid data', async (testData) => {
-      await component.sendContactMessage(testData.name, testData.email, testData.message);
+    )('should not call ContactService with incomplete data', async (testData) => {
+      await component.trySendMessage(testData.name, testData.email, testData.message);
 
-      expect(contactServiceSpy.sendContactMessage).not.toHaveBeenCalled();
+      expect(serviceSpy.sendContactMessage).not.toHaveBeenCalled();
     });
   });
-
-
 });
 
-function createContactServiceSpy() {
-  return {
-    sendContactMessage: vi.fn()
-  };
-}
+
--- web/client/src/app/features/contact/contact-form/contact-form.ts
+++ web/client/src/app/features/contact/contact-form/contact-form.ts
@@ -1,26 +1,28 @@
-import {Component, Inject} from '@angular/core';
-import {ContactService, DIToken} from "../contact-service/contact-service";
+import {Component, Inject, signal} from '@angular/core';
+import {form, FormField, FormRoot} from "@angular/forms/signals";
 import {ValidEmail} from "../contact-service/valid-email";
+import {ContactService, DIToken} from "../contact-service/contact-service";
 
 @Component({
   selector: 'app-contact-form',
+  imports: [FormField, FormRoot],
   template: `
-    <form>
+    <form [formRoot]="contactForm">
       <div>
         <label>
           {{content.nameInput.label}}
-          <input type="text" />
+          <input type="text" [formField]="contactForm.name"/>
         </label>
         <label>
           {{content.emailInput.label}}
-          <input type="text" />
+          <input type="text" [formField]="contactForm.email"/>
         </label>
         <label>
           {{content.messageInput.label}}
-          <textarea></textarea>
+          <textarea [formField]="contactForm.message" rows="4"></textarea>
         </label>
       </div>
-      <button type="submit" class="btn btn-primary">{{content.buttons.submit.label}}</button>
+      <button role="button" type="submit" class="btn btn-primary">{{content.buttons.submit.label}}</button>
     </form>
   `,
   styles: `
@@ -52,12 +54,6 @@ label > input,textarea {
 }`
 })
 export class ContactForm {
-  constructor(@Inject(DIToken) private readonly contactService: ContactService) {
-
-  }
-
-  // TODO service, binding
-
   content = {
     nameInput: {label: 'Naam'},
     emailInput: {label: 'E-mail'},
@@ -69,16 +65,50 @@ export class ContactForm {
     }
   };
 
-  async sendContactMessage(name: string, email: string, message: string) {
+  contactFormModel = signal<ContactFormState>({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  contactForm = form(this.contactFormModel, {
+      submission: {
+        action: async () => {
+          await this.trySendMessage(
+            this.contactForm.name().value(),
+            this.contactForm.email().value(),
+            this.contactForm.message().value()
+          );
+        }
+      }
+    }
+  );
+
+  // TODO real service
+  constructor(@Inject(DIToken) private readonly contactService: ContactService) {}
+
+  public async trySendMessage(name: string, email: string, message: string): Promise<void> {
     const parsedEmail = ValidEmail.parseEmail(email);
     if (this.isEmpty(name) || this.isEmpty(message) || parsedEmail === null) {
       return;
     }
 
     await this.contactService.sendContactMessage(name, parsedEmail, message);
+
+    this.notifySuccess('message sent');
+  }
+
+  public notifySuccess(message: string) {
+    window.alert('success: ' + message);
   }
 
   private isEmpty(str: string) {
     return str.length === 0;
   }
+}
+
+interface ContactFormState {
+  name: string;
+  email: string;
+  message: string;
 }
